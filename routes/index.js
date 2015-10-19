8f697c54cd28c26eb86ea61088e856db6718e35a
Klint Tiley <kctiley@gmail.com>
2015-10-19 11:10:33 -0600

    Signup and signin working with basic validations.

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,67 @@ router.post('/signup', function(req, res, next) {
   if (errors.length > 0)
     res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors})
   else{
-    usersCollection.insert({email: req.body.inputEmail, password: req.body.inputPassword}, function(err, record){
-      res.render('index', { title: 'G Class' , user: "You are signed in."});
+    usersCollection.find({email: req.body.inputEmail}, function(err, record){
+      if(record.length){
+        errors.push('Email not available');
+        res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors})
+      }
+      else{
+        usersCollection.insert({email: req.body.inputEmail, password: req.body.inputPassword}, function(err, record){
+          res.render('index', { title: 'G Class' , subtitle: 'Signed In!', user: "user true"});
+        })
+      }
     })
   }
 });
+
+/* GET SignIn */
+router.get('/signin', function(req, res, next) {
+  res.render('sign', { title: 'G Class' , subtitle: "SignIn", signin: true});
+});
+
+
+/* POST SignIn */
+router.post('/signin', function(req, res, next) {
+  var errors = [];
+  if(!req.body.inputEmail.trim())
+    errors.push('Must enter email')
+  if(!req.body.inputPassword.trim())
+    errors.push('Must enter password')
+  if (errors.length > 0)
+    res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors})
+  else{
+    usersCollection.findOne({email: req.body.inputEmail}, function(err, record){
+      if(!record){
+        errors.push('Record not found. Please check that your email is correct.');
+        res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors, signin:true})
+      }
+      else{
+        if(record.password !== req.body.inputPassword){
+          errors.push('Incorrect password');
+          res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors, signin:true}) 
+        }
+        else{
+          res.render('index', {title: 'G Class', subtitle: 'Signed In!', user: "You're signed in " + req.body.inputEmail})
+        }
+      }
+      // if(record){
+      //   console.log("Record" + record.password)
+      //   if(record.password == req.body.inputPassword){
+      //     res.render('index', {title: 'G Class', subtitle: 'Signed In!', signin: true})
+      //   }
+      //   else{
+      //     errors.push('Incorrect password')
+      //   }
+      // }
+      // else{
+      //   errors.push('Record not found. Please check that your email is correct.')
+      // }
+      // res.render('sign', {title: 'G Class', subtitle: 'Try again', errors: errors, signin:true})
+    })
+  }
+});
+
+
 
 module.exports = router;
