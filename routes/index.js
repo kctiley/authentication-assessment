d12f715e5829fa069c27e33b8182be5eb7cb83be
Klint Tiley <kctiley@gmail.com>
2015-10-19 13:56:16 -0600

    All working

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/authentication-assessment');
 var usersCollection = db.get('usersCollection');
+var studentsCollection = db.get('studentsCollection');
 var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'G Class' });
+  res.render('index');
 });
 
 /* GET SignUp */
 router.get('/signup', function(req, res, next) {
-  res.render('sign', { title: 'G Class'});
+  res.render('sign');
 });
 
 /* POST SignUp */
@@ -22,18 +23,18 @@ router.post('/signup', function(req, res, next) {
   if(!req.body.inputPassword.trim())
     errors.push('Must enter password')
   if (errors.length > 0)
-    res.render('sign', {title: 'G Class - Try again', errors: errors})
+    res.render('sign', {signInPage: true, errors: errors})
   else{
     usersCollection.find({email: req.body.inputEmail}, function(err, record){
       if(record.length){
         errors.push('Email not available');
-        res.render('sign', {title: 'G Class - Try again', errors: errors})
+        res.render('sign', {errors: errors})
       }
       else{
         req.session.email = req.body.inputEmail;
         var hashedPassword = bcrypt.hashSync(req.body.inputPassword, 8);
         usersCollection.insert({email: req.body.inputEmail, hashedPassword: hashedPassword}, function(err, record){
-          res.render('index', { title: 'G Class', statusSignedIn: true, session_id: req.session.email});
+          res.render('index', { session_id: req.session.email});
         })
       }
     })
@@ -42,7 +43,7 @@ router.post('/signup', function(req, res, next) {
 
 /* GET SignIn */
 router.get('/signin', function(req, res, next) {
-  res.render('sign', { title: 'G Class', signinPage: true});
+  res.render('sign', { signInPage: true});
 });
 
 
@@ -54,21 +55,21 @@ router.post('/signin', function(req, res, next) {
   if(!req.body.inputPassword.trim())
     errors.push('Must enter password')
   if (errors.length > 0)
-    res.render('sign', {title: 'G Class - Try again', errors: errors})
+    res.render('sign', { errors: errors})
   else{
     usersCollection.findOne({email: req.body.inputEmail}, function(err, record){
       if(!record){
         errors.push('Record not found. Please check that your email is correct.');
-        res.render('sign', {title: 'G Class - Try again', errors: errors, signinPage:true})
+        res.render('sign', {errors: errors, signInPage:true})
       }
       else{
         if(bcrypt.compareSync(req.body.inputPassword, record.hashedPassword)){
           req.session.email = req.body.inputEmail;
-          res.render('index', {title: 'G Class', session_id: req.session.email, statusSignedIn: true})
+          res.render('index', {session_id: req.session.email})
         }
         else{
           errors.push('Incorrect password');
-          res.render('sign', {title: 'G Class - Try again', errors: errors, signinPage:true}) 
+          res.render('sign', {errors: errors, signInPage:true}) 
         }
       }
     })
@@ -81,6 +82,27 @@ router.get('/logout', function(req, res, next){
   res.redirect('/')
 })
 
+router.get('/new', function(req, res, next){
+  res.render('new', {session_id: req.session.email})
+})
+
+router.post('/new', function(req, res, next){
+  studentsCollection.insert({name: req.body.inputName, phone: req.body.inputPhone}, function(err, record){
+    res.render('index', {statusSignedIn: true,  session_id: req.session.email})
+  })
+})
+
+router.get('/all_students', function(req, res, next){
+  studentsCollection.find({}, function(err, record){
+    res.render('students', {session_id: req.session.email, allStudents: record})
+  })
+})
+
+router.get('/students/:name', function(req, res, next){
+  studentsCollection.findOne({name: req.params.name}, function(err, record){
+    res.render('profile', {session_id: req.session.email, profile: record})
+  })
+})
 
 
 
